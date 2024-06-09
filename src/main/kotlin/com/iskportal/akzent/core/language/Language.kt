7572d01ecd8a35894fdb868f762741b7d111d737
Kid A <me@thisismeamir.com>
2024-06-09 13:19:49 +0330

    [Implementation] Language Implementation.

--- src/main/kotlin/com/iskportal/akzent/core/language/Language.kt
+++ src/main/kotlin/com/iskportal/akzent/core/language/Language.kt
@@ -1,5 +1,8 @@
 package com.iskportal.akzent.core.language
 
+import com.iskportal.akzent.core.language.utils.GrammarCorrelation
+import com.iskportal.akzent.core.language.utils.WordCorrelation
+import com.iskportal.akzent.core.language.utils.basicWordCorrelations
 import com.iskportal.akzent.core.phonetic.Phonetic
 import com.iskportal.akzent.core.phonetic.Utterance
 
@@ -11,8 +14,12 @@ interface Language {
 
     val utterances: List<Utterance>
     val words: List<Word>
+    val grammar: List<Grammar>
 
-    // TODO Grammar Definition
+    val wordCorrelations: List<WordCorrelation>
+    val grammarCorrelation: List<GrammarCorrelation>
+
+    // TODO Synonym Antonym and Correlation Implementation
 
 
     fun generateUtterance(): Utterance
